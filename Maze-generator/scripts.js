4bf5e7530bb72ea29e8b0417152a474a62803247
avvinci <vinciabhinav7@gmail.com>
2019-04-21 14:06:30 +0530

    set logic not working

--- Maze-generator/scripts.js
+++ Maze-generator/scripts.js
@@ -1,5 +1,5 @@
-let height = 5 ; 
-let width = 5; 
+let height = 3  ; 
+let width = 3; 
 
 height *= 4 ; 
 width *= 4 ; 
@@ -9,10 +9,17 @@ let neighbourArray = [[0,-1], [0,1], [1,0], [-1,0]] ;
 let arr = [] ; 
 let available = new Set() ; 
 
-function createBlock(par){
+function inGrid(p){
+    if(p[0] >= 0 && p[1] >= 0 && p[0] < height && p[1] < width) 
+        return true; 
+    return false ; 
+}
+
+function createBlock(par,i,j){
 
     let elem  = document.createElement('div');
     elem.setAttribute('class', 'block wine');
+    elem.setAttribute('id' , 'block' + i + ' ' +  j); 
     par.appendChild(elem);
 }
 
@@ -32,23 +39,40 @@ for(let i=0;i<height;i++){
     for(let j= 0 ;j < width ; j++ ){
         temp.push(0) ;
         available.add([i,j]) ; 
-        createBlock(par) ;  
+        createBlock(par,i,j) ;  
     }
     arr.push(temp) ; 
 }
+
+// console.log(available);  
 
 while(available.size > 0 ){
 
     const iterator1 = available[Symbol.iterator]();
     let cell = iterator1.next().value ; 
-    console.log(cell) ; 
-    available.delete(cell) ; 
-    // for(let i =0;i < neighbours ;i++ ){
-    //     let newcell = cell + neighbourArray[i];
-    //     if(available.has(newcell)){
-    //         available.delete(newcell); 
-    //     } 
+    let count = 0 ; 
+    while(true){
+        count++ ; 
+        if(count > 100 ) break ;  
+        console.log(cell) ; 
+        console.log(available.has(cell)); 
+        available.delete(cell) ; 
+        let ind = [] ; 
+        for(let i =0;i < neighbours ;i++ ){
+            let newcell = [cell[0] + neighbourArray[i][0] , cell[1] + neighbourArray[i][1] ] ;
+            console.log('newcell', newcell , available.has(cell));
+            if( inGrid(newcell) && available.has(newcell)){
+                ind.push(i) ; 
+                // available.delete(newcell); 
+            } 
+        }
+        console.log(ind.length);
 
-    // }
+        if(ind.length == 0 ) break ; 
+
+        let rind = Math.random(ind.length) ; 
+        cell =  [cell[0] + neighbourArray[rind][0] , cell[1] + neighbourArray[rind][1] ] ; 
+    }
 }
-console.log(available); 
+
+// console.log(available); 
