e96ae603d51590979c73ef5f05ab804b8b87d855
avvinci <vinciabhinav7@gmail.com>
2019-04-22 19:54:25 +0530

    createCustom maze not working

--- Maze-generator/scripts.js
+++ Maze-generator/scripts.js
@@ -1,14 +1,87 @@
-let height = 3  ; 
-let width = 3; 
-
-height *= 4 ; 
-width *= 4 ; 
-
 let neighbours = 4 ;
 let neighbourArray = [[0,-1], [0,1], [-1,0], [1,0]] ;  
 let arr = [] ; 
+let createButton = document.getElementById('createButton');  
+createButton.onclick = createCustomMaze ; 
 
-function inGrid(x,y){
+function createCustomMaze(){
+    let height = 3  ; 
+    let width = 3; 
+    height *= 4 ; 
+    width *= 4 ; 
+    removeOldMaze(height); 
+
+    createMaze(height,width) ; 
+}
+
+function removeOldMaze(height){
+    arr = [] ; 
+    for(let i=0;i<height;i++){
+        // removeRow(i) ; 
+    }
+}
+
+function removeRow(i){
+    let cRow = document.getElementById('Row'+i) ; 
+    document.body.removeChild(cRow) ; 
+}
+
+function createMaze(height , width){
+
+    for(let i=0;i<height;i++){
+        let temp =[] ; 
+        let par  = createRow(i) ; 
+    
+        for(let j= 0 ;j < width ; j++ ){
+            temp.push(0) ;
+            createBlock(par,i,j) ;  
+        }
+        arr.push(temp) ; 
+    }
+   
+    let freeCount = height*width ; 
+    let st = []; 
+    st.push({x:0,y:0}) ;
+    let st_size = 1 ; 
+
+    while(freeCount > 0 && st_size > 0  ){
+        cell = st[st_size-1] ; 
+        // console.log(cell) ; 
+
+        if(arr[cell.x][cell.y] === 0 ) 
+            freeCount-- ; 
+        arr[cell.x][cell.y] = 1; 
+
+        let ind = [] ; 
+        for(let i =0;i < neighbours ;i++ ){
+            let newcell = { x : cell.x + neighbourArray[i][0] , y: cell.y + neighbourArray[i][1] } ;
+            // console.log('newcell', newcell,  inGrid(newcell) );
+            if( inGrid(newcell.x , newcell.y,height,width) && (arr[newcell.x][newcell.y]  === 0)  ){
+                ind.push(i) ; 
+                // console.log('newcell', newcell , arr[newcell.x][newcell.y]);
+            } 
+        }
+
+        if(ind.length == 0 ) {
+            st_size--;
+            continue ; 
+        }
+        let rind = Math.floor((Math.random() * ind.length )) ; 
+        let nextcell =  {x : cell.x  + neighbourArray[ind[rind]][0] , y :cell.y + neighbourArray[ind[rind]][1] } ; 
+        removeBorders(cell, nextcell,rind,ind) ; 
+
+        if(st_size >= st.length)
+            st.push(nextcell) ; 
+        else
+            st[st_size] = nextcell ;
+        st_size++ ; 
+
+    }
+
+}
+
+
+function inGrid(x,y ,height ,width){
     if(x >= 0 && y >= 0 && x < height && y < width) 
         return true; 
     return false ; 
@@ -17,7 +90,7 @@ function hasNeighbourVisited(x,y){
     for(let i =0 ; i< neighbours ;i++ ){
         let nx = x +neighbourArray[i] ; 
         let ny  = y + neighbourArray[i] ; 
-        if( inGrid(nx,ny)  && arr[nx][ny] ===  1) return true ;   
+        if( inGrid(nx,ny,height,width)  && arr[nx][ny] ===  1) return true ;   
     }
     return false; 
 }
@@ -30,25 +103,16 @@ function createBlock(par,i,j){
     par.appendChild(elem);
 }
 
-function createRow(){
+function createRow(i){
     let par = document.createElement('div');
     par.setAttribute('class', 'container');
+    par.setAttribute('id', 'Row' + i );
     // console.log(par); 
     document.body.appendChild(par);
     return par; 
 }
 
 
-for(let i=0;i<height;i++){
-    let temp =[] ; 
-    let par  = createRow() ; 
-
-    for(let j= 0 ;j < width ; j++ ){
-        temp.push(0) ;
-        createBlock(par,i,j) ;  
-    }
-    arr.push(temp) ; 
-}
 
 function removeBorders(cell,nextcell ,rind,ind ){
 
@@ -82,41 +146,10 @@ function removeBorders(cell,nextcell ,rind,ind ){
 }
 
 
-let freeCount = height*width ; 
-let st = []; 
-st.push({x:0,y:0}) ;
-let st_size = 1 ; 
-
-while(freeCount > 0 && st_size > 0  ){
-    cell = st[st_size-1] ; 
-    // console.log(cell) ; 
-
-    if(arr[cell.x][cell.y] === 0 ) 
-        freeCount-- ; 
-    arr[cell.x][cell.y] = 1; 
-
-    let ind = [] ; 
-    for(let i =0;i < neighbours ;i++ ){
-        let newcell = { x : cell.x + neighbourArray[i][0] , y: cell.y + neighbourArray[i][1] } ;
-        // console.log('newcell', newcell,  inGrid(newcell) );
-        if( inGrid(newcell.x , newcell.y) && (arr[newcell.x][newcell.y]  === 0)  ){
-            ind.push(i) ; 
-            // console.log('newcell', newcell , arr[newcell.x][newcell.y]);
-        } 
-    }
-
-    if(ind.length == 0 ) {
-        st_size--;
-        continue ; 
-    }
-    let rind = Math.floor((Math.random() * ind.length )) ; 
-    let nextcell =  {x : cell.x  + neighbourArray[ind[rind]][0] , y :cell.y + neighbourArray[ind[rind]][1] } ; 
-    removeBorders(cell, nextcell,rind,ind) ; 
-
-    if(st_size >= st.length)
-        st.push(nextcell) ; 
-    else
-        st[st_size] = nextcell ;
-    st_size++ ; 
-
+window.onload = function(){
+    let height = 3  ; 
+    let width = 3; 
+    height *= 4 ; 
+    width *= 4 ; 
+    createMaze(height,width) ; 
 }
